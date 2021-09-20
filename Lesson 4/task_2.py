78c825963953c22d91cd85cb8c281f8704ed5190
1LHOM <75044532+1LHOM@users.noreply.github.com>
2021-09-20 15:02:04 +0300

    Update task_2
    Added standard algorithm of Eratosthenes

--- Lesson 4/task_2.py
+++ Lesson 4/task_2.py
@@ -68,3 +68,46 @@ print(len(with_algorithm_of_eratosthenes(1000000)))  # 78498
 #         1    0.000    0.000    0.000    0.000 {built-in method math.isqrt}
 #         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
 
+
+# Алгоритм Эратосфена из методичке:
+
+
+def standard_algorithm_of_eratosthenes(n):
+    a = [0] * n  # создание массива с n количеством элементов
+    for i in range(n):  # заполнение массива ...
+        a[i] = i  # значениями от 0 до n-1
+    # вторым элементом является единица, которую не считают простым числом
+    # забиваем ее нулем.
+    a[1] = 0
+    m = 2  # замена на 0 начинается с 3-го элемента (первые два уже нули)
+    while m < n:  # перебор всех элементов до заданного числа
+        if a[m] != 0:  # если он не равен нулю, то
+            j = m * 2  # увеличить в два раза (текущий элемент - простое число)
+            while j < n:
+                a[j] = 0  # заменить на 0
+                j = j + m  # перейти в позицию на m больше
+        m += 1
+    # вывод простых чисел на экран (может быть реализован как угодно)
+    b = []
+    for i in a:
+        if a[i] != 0:
+            b.append(a[i])
+    del a
+    return b
+
+
+print(cProfile.run("standard_algorithm_of_eratosthenes(1000000)"))
+print(len(standard_algorithm_of_eratosthenes(1000000)))  # 78498
+
+# cProfile test results for 1 million numbers:
+
+#          78502 function calls in 1.137 seconds
+#
+#    Ordered by: standard name
+#
+#    ncalls  tottime  percall  cumtime  percall filename:lineno(function)
+#         1    0.007    0.007    1.136    1.136 <string>:1(<module>)
+#         1    1.114    1.114    1.129    1.129 task_2.py:75(standard_algorithm_of_eratosthenes)
+#         1    0.000    0.000    1.137    1.137 {built-in method builtins.exec}
+#     78498    0.015    0.000    0.015    0.000 {method 'append' of 'list' objects}
+#         1    0.000    0.000    0.000    0.000 {method 'disable' of '_lsprof.Profiler' objects}
